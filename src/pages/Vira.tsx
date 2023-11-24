899038edcdbeb4fdf3a93075420158138a347e61
MrIsdi <isdiridho@gmail.com>
2023-11-24 23:11:01 +0700

    Up

--- src/pages/Vira.tsx
+++ src/pages/Vira.tsx
@@ -8,14 +8,14 @@ export default function Vira(){
         <>
             <div className="bg-gradient-to-b from-[#00000087] to-teal-800 h-screen">
                 <div className="flex flex-col items-center">
-                    <h1 className="text-white md:text-4xl text-2xl my-8">You are invited to</h1>
+                    <h1 className="text-white md:text-4xl text-2xl md:my-8 my-0 md:mt-0 mt-16">You are invited to</h1>
                     <h2 className="font-['My_Soul'] md:text-7xl text-5xl text-white mb-8">Graduation Party</h2>
                     <h2 className="font-['My_Soul'] md:text-7xl text-5xl text-white mb-12">Of</h2>
                     <h3 className="text-white md:text-5xl text-3xl">Delvira Syafna, S. Si</h3>
                 </div>
                 <div className="text-white flex flex-col justify-center items-center my-12">
-                    <p className="text-2xl text-center">Dear you,</p>
-                    <p className="mt-4 md:mb-8 mb-16 md:text-2xl text-xl flex justify-center items-center md:w-[488px] md:px-0 px-12 h-[55px] rounded-[20px] bg-[rgba(10,1,1,0.20)] backdrop-blur-[5px] border-[2px] border-white">Alif dan Partner</p>
+                    <p className="md:text-2xl text-base text-center">Dear you,</p>
+                    <p className="mt-4 md:mb-8 mb-16 md:text-2xl text-base flex justify-center items-center md:w-[488px] md:px-0 px-12 h-[55px] rounded-[20px] bg-[rgba(10,1,1,0.20)] backdrop-blur-[5px] border-[2px] border-white">Alif dan Partner</p>
                     <button className="w-[261px] h-[59px] bg-cover bg-no-repeat bg-[url(https://cdn.animaapp.com/projects/6560c42e4e4ddfb844c5cfc1/releases/6560c497b9e96f16e727a1ec/img/calendar.svg)]">Open Invitation</button>
                 </div>
             </div>
